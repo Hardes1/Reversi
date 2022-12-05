d600d8a0f09c3afd22b1b283cd8a26b10511b861
Ustinov George <ustinovgo@gmail.com>
2022-12-05 02:55:48 +0200

    FIX CE

--- src/main/java/ru/ustinov/CommonConstants.java
+++ src/main/java/ru/ustinov/CommonConstants.java
@@ -1,9 +1,5 @@
 package ru.ustinov;
 
 public class CommonConstants {
-    private CommonConstants() {
-        throw new IllegalStateException();
-    }
-
     public static final String INCORRECT_COMMAND = "Была введена некорректная команда, попробуйте ещё раз.";
 }
--- src/main/java/ru/ustinov/interactor/InteractorConstants.java
+++ src/main/java/ru/ustinov/interactor/InteractorConstants.java
@@ -3,11 +3,15 @@ package ru.ustinov.interactor;
 import ru.ustinov.CommonConstants;
 
 final class InteractorConstants extends CommonConstants {
-    final static String PARTING = "Спасибо за игру, возвращайтесь снова!";
+    private InteractorConstants() {
+        throw new IllegalStateException();
+    }
 
-    final static String GREETING = "Приветствую вас в моей реализации игры \"Реверси\"!";
+    static final String PARTING = "Спасибо за игру, возвращайтесь снова!";
 
-    final static String HELP = "Вам доступны следующие команды для ввода:\n" +
+    static final String GREETING = "Приветствую вас в моей реализации игры \"Реверси\"!";
+
+    static final String HELP = "Вам доступны следующие команды для ввода:\n" +
             String.format("\"%s\" - начать новую игру.\n", Command.PLAY) +
             String.format("\"%s\" - посмотреть это меню ещё раз.\n", Command.HELP) +
             String.format("\"%s\" - вывести наилучший рейтинг за сессию.\n", Command.RATING) +
